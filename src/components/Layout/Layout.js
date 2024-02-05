6cd80c866d69bc78eb4abdd41258a3807126af51
Nishant Malik <nishantmalik2015@.com>
2024-02-06 02:54:04 +0530

    worked on footer some design issues

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import { Col, Row } from 'antd';
+// import { Col, Row } from 'antd';
 import TopCarousel from '../TopCarousel/TopCarousel';
 import TopPickings from '../TopPickings/TopPickings.';
 import TopOffers from '../TopOffers/TopOffers';
+import Sections from '../Sections/Sections';
 
 function Layout() {
+  const SECTION_INFO = [
+    {
+      sectionName: 'FOR HER',
+      imagesUrl: [
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-women/blue_shirt_girl.jpeg',
+      ],
+    },
+    {
+      sectionName: 'FOR HIM',
+      imagesUrl: [
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-men/kurta_men.jpeg',
+      ],
+    },
+    {
+      sectionName: 'FOR KIDS',
+      imagesUrl: [
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+        'https://ecomm.dotvik.com/dist/images/homePageRoundImg-kids/kids_shorts.jpeg',
+      ],
+    },
+  ];
+
   return (
     <>
       {/* <Row> */}
@@ -14,6 +57,9 @@ function Layout() {
       <TopCarousel />
       <TopPickings />
       <TopOffers />
+      {SECTION_INFO.map((info) => (
+        <Sections info={info} />
+      ))}
       {/* </Col> */}
       {/* </Row> */}
     </>
--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
-function Sections() {
+import { Row, Col } from 'antd';
+import React from 'react';
+import '../TopOffers/TopOffers.css';
+function Sections(props) {
+  const { info } = props;
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Row>
+        <Col sm={24} md={24}>
+          <div className='center-div border-btm'>{info.sectionName}</div>
+        </Col>
+      </Row>
+      <Row justify='space-evenly'>
+        {info.imagesUrl.map((imageUrl, i) => {
+          return (
+            <Col className='center-div' key={imageUrl + i} span={6}>
+              <img
+                style={{
+                  width: '70%',
+                  height: 'auto',
+                  maxWidth: '100%',
+                }}
+                src={imageUrl}
+                alt='loding'
+              ></img>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
 }
 
-export default Sections
+export default Sections;
